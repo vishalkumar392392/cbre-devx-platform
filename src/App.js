c1d431ea114d582e68d6244fbbda35912cbb5eeb
vishal palla <vishalpalla27@gmail.com>
2021-03-02 15:09:19 +0530

    added header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import "./App.css";
-import Sidebar from "./components/Sidebar/Sidebar";
 import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div>
-      <Sidebar />
       <Header />
     </div>
   );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,53 @@
 import React from "react";
-
+import { NavLink } from "react-router-dom";
+import "./Header.css";
 function Header(props) {
-  return <div>Header</div>;
+  return (
+    <div>
+      <div className="header">
+        <h2>Paree looys, Guest!</h2>
+        <h6>CBRE Service Catalog</h6>
+      </div>
+
+      <nav className="navbar navbar-expand-sm navbar-light bg-light">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <NavLink className="nav-link links" to="/services">
+                Services
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link links" to="/libraries">
+                Libraries
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link links" to="/ux">
+                UX
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link links" to="/frameworks">
+                Frameworks
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
 }
 
 export default Header;
